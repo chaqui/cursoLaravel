b9b7633abfc68b92766d8ef51f68d42e84311fc8
Josue Chaqui <josue.chaqui@gmail.com>
2019-06-05 14:06:39 -0600

    agregando request

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index');
 
-Route::get('/test', 'HomeController@index');
+Route::get('/dashboard', 'DashboardController@index');
